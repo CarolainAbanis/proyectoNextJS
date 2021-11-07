f9d2ec8bcd2b5f5686cd4c4fb4c7a7404b2b0af0
Carolain <carolainabanis@gmail.com>
2021-11-07 17:30:12 -0500

    agregar tailwind y rutas de muestra

---
+++ pages/carol.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const carol = () => {
+    return(
+        <div>
+            hola carol, bienvenida al mundo cruel
+        </div>
+
+    )
+
+}
+export default carol
